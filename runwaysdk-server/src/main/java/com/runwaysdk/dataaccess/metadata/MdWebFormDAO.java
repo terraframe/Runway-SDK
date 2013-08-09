e898fc725f0679e93d46568134da0f0382091e30
justin <jnaifeh@terraframe.com>
2013-08-09 09:29:18 -0400

    Fixed MdForm API to return flattened MdFields recursively.

--- runwaysdk-server/src/main/java/com/runwaysdk/dataaccess/metadata/MdWebFormDAO.java
+++ runwaysdk-server/src/main/java/com/runwaysdk/dataaccess/metadata/MdWebFormDAO.java
@@ -18,8 +18,6 @@
  ******************************************************************************/
 package com.runwaysdk.dataaccess.metadata;
 
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -27,10 +25,18 @@ import java.util.Map;
 
 import com.runwaysdk.constants.WebFormFieldInfo;
 import com.runwaysdk.dataaccess.BusinessDAO;
+import com.runwaysdk.dataaccess.BusinessDAOIF;
+import com.runwaysdk.dataaccess.MdFieldDAOIF;
 import com.runwaysdk.dataaccess.MdWebFieldDAOIF;
 import com.runwaysdk.dataaccess.MdWebFormDAOIF;
 import com.runwaysdk.dataaccess.RelationshipDAOIF;
 import com.runwaysdk.dataaccess.attributes.entity.Attribute;
+import com.runwaysdk.query.BusinessDAOQuery;
+import com.runwaysdk.query.OIterator;
+import com.runwaysdk.query.QueryFactory;
+import com.runwaysdk.query.RelationshipDAOQuery;
+import com.runwaysdk.system.metadata.MdWebField;
+import com.runwaysdk.system.metadata.WebGroupField;
 
 
 public class MdWebFormDAO extends MdFormDAO implements MdWebFormDAOIF
@@ -84,19 +90,55 @@ public class MdWebFormDAO extends MdFormDAO implements MdWebFormDAOIF
       fields.add((MdWebFieldDAOIF)rel.getChild());
     }
     
-    //Sort the MdParamters into ascending order by the parameter order
-    Collections.sort(fields, new Comparator<MdWebFieldDAOIF>()
-    {
-      public int compare(MdWebFieldDAOIF f1, MdWebFieldDAOIF f2)
-      {
-        Integer o1 = Integer.parseInt(f1.getFieldOrder());
-        Integer o2 = Integer.parseInt(f2.getFieldOrder());
-
-        return o1.compareTo(o2);
-      }
-    });
-    
     return fields;
+  }
+  
+  public List<? extends MdFieldDAOIF> getOrderedMdFields()
+  {
+    QueryFactory f = new QueryFactory();
+    BusinessDAOQuery q = f.businessDAOQuery(MdWebField.CLASS);
+    BusinessDAOQuery q1 = f.businessDAOQuery(MdWebField.CLASS);
+    RelationshipDAOQuery relQ = f.relationshipDAOQuery(WebGroupField.CLASS);
+    
+    // exclude fields that are directly beneath a group
+    relQ.WHERE(relQ.childId().EQ(q1.id()));
+    q.AND(q.isNotChildIn_SUBSELECT(relQ));
+    
+
+    q.WHERE(q.aReference(MdWebField.DEFININGMDFORM).EQ(this));
+    q.ORDER_BY_ASC(q.aInteger(MdWebField.FIELDORDER));
+    
+    
+    OIterator<? extends BusinessDAOIF> iterator = q.getIterator();
+
+    try
+    {
+      List<MdFieldDAOIF> allFields = new LinkedList<MdFieldDAOIF>();
+      @SuppressWarnings("unchecked")
+      List<? extends MdWebFieldDAO> fields = (List<? extends MdWebFieldDAO>) iterator.getAll();
+      
+      recurseFields(allFields, fields);
+
+      return allFields;
+    }
+    finally
+    {
+      iterator.close();
+    }
+  }
+  
+  private void recurseFields(List<MdFieldDAOIF> allFields, List<? extends MdFieldDAOIF> fields)
+  {
+    for(MdFieldDAOIF field : fields)
+    {
+      allFields.add(field);
+
+      if(field instanceof MdWebGroupDAO)
+      {
+        MdWebGroupDAO group = (MdWebGroupDAO) field;
+        recurseFields(allFields, group.getGroupFields());
+      }
+    }    
   }
 
   @Override
