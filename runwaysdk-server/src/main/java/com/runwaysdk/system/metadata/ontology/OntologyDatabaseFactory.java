dee7d72fe0b0dac0e19c2f259660e4a320d30ebb
jsmethie <jsmethie@terraframe.com>
2013-10-29 09:15:53 -0600

    Checkin of OntologyDatabaseFactory files

---
+++ runwaysdk-server/src/main/java/com/runwaysdk/system/metadata/ontology/OntologyDatabaseFactory.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.runwaysdk.system.metadata.ontology;
+
+import com.runwaysdk.business.ontology.OntologyStrategyIF;
+import com.runwaysdk.dataaccess.ProgrammingErrorException;
+import com.runwaysdk.dataaccess.database.general.AbstractDatabase;
+import com.runwaysdk.dataaccess.database.general.PostgreSQL;
+
+/*******************************************************************************
+ * Copyright (c) 2013 TerraFrame, Inc. All rights reserved.
+ * 
+ * This file is part of Runway SDK(tm).
+ * 
+ * Runway SDK(tm) is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * Runway SDK(tm) is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Runway SDK(tm). If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+public class OntologyDatabaseFactory
+{
+
+  /**
+   * @param database
+   * @param strategy
+   */
+  public OntologyDatabase getInstance(AbstractDatabase database, OntologyStrategyIF strategy)
+  {
+    if (strategy instanceof DatabaseAllPathsStrategy && database instanceof PostgreSQL)
+    {
+      return new PostgresOntolgoyDatabase();
+    }
+
+    throw new ProgrammingErrorException("Database implementation doesn't exist for the database [" + database.getClass().getName() + "] and strategy [" + strategy.getClass().getName() + "]");
+  }
+}
