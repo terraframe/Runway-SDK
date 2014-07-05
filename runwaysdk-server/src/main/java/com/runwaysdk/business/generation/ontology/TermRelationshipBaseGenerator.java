f7b2a588d756f878560bccda14306e71158e48cc
rrowlands <rrowlands@terraframe.com>
2014-07-04 19:48:38 -0600

    Comitting files that were not committed in last commit because the root gitignore had all projects on ignore.

---
+++ runwaysdk-server/src/main/java/com/runwaysdk/business/generation/ontology/TermRelationshipBaseGenerator.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2013 TerraFrame, Inc. All rights reserved.
+ * 
+ * This file is part of Runway SDK(tm).
+ * 
+ * Runway SDK(tm) is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * Runway SDK(tm) is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Runway SDK(tm). If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package com.runwaysdk.business.generation.ontology;
+
+import com.runwaysdk.business.generation.RelationshipBaseGenerator;
+import com.runwaysdk.business.ontology.TermRelationship;
+import com.runwaysdk.dataaccess.MdClassDAOIF;
+import com.runwaysdk.dataaccess.MdRelationshipDAOIF;
+
+public class TermRelationshipBaseGenerator extends RelationshipBaseGenerator
+{
+  public TermRelationshipBaseGenerator(MdRelationshipDAOIF mdRelatIF)
+  {
+    super(mdRelatIF);
+  }
+
+  @Override
+  protected String getExtends(MdClassDAOIF parent)
+  {
+    if (parent == null)
+    {
+      return TermRelationship.class.getName();
+    }
+    else
+    {
+      return getParentClass(parent);
+    }
+  }
+}
---
+++ runwaysdk-test/src/main/domain/ddl/DeterministicTermRelationship.sql
@@ -0,0 +1,4 @@
+UPDATE md_entity SET has_deterministic_ids= 1 , id= '8ur2ybsvotytfkh9zedkv3n8v8gtzfx700000000000000000000000000000001'  WHERE id='8ur2ybsvotytfkh9zedkv3n8v8gtzfx700000000000000000000000000000001';
+UPDATE metadata SET seq= 4795 , last_update_date= '2014-07-03 09:27:04'  WHERE id='8ur2ybsvotytfkh9zedkv3n8v8gtzfx700000000000000000000000000000001';
+UPDATE md_entity SET has_deterministic_ids= 1 , id= '8ur2ybsvotytfkh9zedkv3n8v8gtzfx700000000000000000000000000000001'  WHERE id='8ur2ybsvotytfkh9zedkv3n8v8gtzfx700000000000000000000000000000001';
+UPDATE metadata SET seq= 4796 , last_update_date= '2014-07-03 09:27:04'  WHERE id='8ur2ybsvotytfkh9zedkv3n8v8gtzfx700000000000000000000000000000001';
