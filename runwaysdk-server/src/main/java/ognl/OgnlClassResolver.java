18402711b2e62c80a16658b443941010edf9ff76
nmceachen <nmceachen@terraframe.com>
2014-12-22 21:39:08 -0700

    Implemented a workaround to an Ongl bug where it was caching reflection Method objects, which was causing problems with reloading classes.

---
+++ runwaysdk-server/src/main/java/ognl/OgnlClassResolver.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2013 TerraFrame, Inc. All rights reserved. 
+ * 
+ * This file is part of Runway SDK(tm).
+ * 
+ * Runway SDK(tm) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * Runway SDK(tm) is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Runway SDK(tm).  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package ognl;
+
+import java.util.Map;
+
+import ognl.ClassResolver;
+
+import com.runwaysdk.generation.loader.LoaderDecorator;
+
+// Heads up: remove class
+public class OgnlClassResolver implements ClassResolver
+{
+
+  /* (non-Javadoc)
+   * @see ognl.ClassResolver#classForName(java.lang.String, java.util.Map)
+   */
+  @Override
+  public Class classForName(String className, Map context) throws ClassNotFoundException
+  {
+    return LoaderDecorator.load(className);
+  }
+  
+  /** 
+   * Clears the cache of reflected methods in Ognl.
+   */
+  public static void clearOgnlRuntimeMethodCache()
+  {
+    synchronized (OgnlRuntime.cacheGetMethod)
+    {
+      OgnlRuntime.cacheGetMethod.clear();
+    }
+  }
+  
+  /** 
+   * Returns the count of cached reflected methods in Ognl.
+   */
+  public static int methodCacheCount()
+  {
+    synchronized (OgnlRuntime.cacheGetMethod)
+    {
+     return OgnlRuntime.cacheGetMethod.size();
+    }
+  }
+  
+}
