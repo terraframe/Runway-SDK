a2ad6d086a2f18ffb19b191bd35e10fd3bac5e37
rrowlands <rrowlands@terraframe.com>
2016-09-21 19:31:41 -0600

    Added test project to runway module so that the version numbers get updated by mvn release plugin.

--- runwaysdk-test/src/test/java/com/runwaysdk/UeberTS.java
+++ runwaysdk-test/src/test/java/com/runwaysdk/UeberTS.java
@@ -22,7 +22,7 @@ import junit.framework.Test;
 import junit.framework.TestResult;
 import junit.framework.TestSuite;
 
-public class UeberTestSuite_MavenTest extends TestSuite
+public class UeberTS extends TestSuite
 {
   @Override
   public void run(TestResult testResult)
