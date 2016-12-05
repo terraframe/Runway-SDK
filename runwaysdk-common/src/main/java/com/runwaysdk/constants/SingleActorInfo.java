e41452a071aba93bb54d2f9a28fd1d79ac893f62
Justin Smethie <jsmethie@terraframe.com>
2016-12-05 10:56:11 -0700

    * Client Session Login Refactor

---
+++ runwaysdk-common/src/main/java/com/runwaysdk/constants/SingleActorInfo.java
@@ -0,0 +1,16 @@
+package com.runwaysdk.constants;
+
+public class SingleActorInfo
+{
+
+  /**
+   * The type name of the users class
+   */
+  public static final String TYPENAME = "SingleActor";
+
+  /**
+   * The type of the users class
+   */
+  public static final String CLASS = Constants.SYSTEM_PACKAGE +"."+TYPENAME;
+
+}
