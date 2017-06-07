fd8bee9d40433e2ce2c6139ad15dcf384d59b51b
nmceachen <nmceachen@terraframe.com>
2017-06-07 17:06:04 -0600

    Created a Ratio Operation Enumeration

---
+++ runwaysdk-common/src/main/java/com/runwaysdk/constants/MathOperations.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2015 TerraFrame, Inc. All rights reserved.
+ *
+ * This file is part of Runway SDK(tm).
+ *
+ * Runway SDK(tm) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Runway SDK(tm) is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Runway SDK(tm).  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.runwaysdk.constants;
+
+public interface MathOperations extends EnumerationMasterInfo
+{
+  public static final String CLASS_NAME       = "MathOperations";
+  
+  public static final String CLASS            = Constants.SYSTEM_PACKAGE+"."+CLASS_NAME;
+  
+  public static final String TABLE            = "math_operations";
+
+  public static final String ID_VALUE         = "i7jxaxmihc67tw2f841s6k02ugere0p600000000000000000000000000000001";
+  
+  
+  public static final String OPERATION        = "operationSymbol";
+  
+  
+  public static final String RATIO_CLASS_NAME = "RatioMathOperations";
+  
+  public static final String RATIO_CLASS      = Constants.SYSTEM_PACKAGE+"."+RATIO_CLASS_NAME;
+  
+  public static final String RATIO_CLASS_ID_VALUE   = "ify4svz61kxr8i8w1lctcgbs1oprhra300000000000000000000000000000196";
+  
+  // Division Operation ID:
+  // icqocsto8e4zkraohiotd68cj2x0sjvpi7jxaxmihc67tw2f841s6k02ugere0p6
+  
+}
---
+++ runwaysdk-server/src/main/java/com/runwaysdk/system/MathOperations.java
@@ -0,0 +1,12 @@
+package com.runwaysdk.system;
+
+public class MathOperations extends MathOperationsBase
+{
+  private static final long serialVersionUID = -1717711916;
+  
+  public MathOperations()
+  {
+    super();
+  }
+  
+}
