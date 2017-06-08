ea848e0a48054f11ef81ef87ff6e71357ba10bca
nmceachen <nmceachen@terraframe.com>
2017-06-07 20:59:52 -0600

    Renamed the ratio data types that I had created earlier

---
+++ runwaysdk-server/src/main/java/com/runwaysdk/system/Operators.java
@@ -0,0 +1,12 @@
+package com.runwaysdk.system;
+
+public class Operators extends OperatorsBase
+{
+  private static final long serialVersionUID = 432345962;
+  
+  public Operators()
+  {
+    super();
+  }
+  
+}
