33f999f9219f7ac1bc7134032d9965dea4e2755e
Justin Smethie <jsmethie@terraframe.com>
2017-11-13 08:46:47 -0700

    Added newInstance property to serialized basic JSON

--- runwaysdk-client/src/main/java/com/runwaysdk/mvc/conversion/MutableDTOToBasicJSON.java
+++ runwaysdk-client/src/main/java/com/runwaysdk/mvc/conversion/MutableDTOToBasicJSON.java
@@ -3,20 +3,22 @@
  *
  * This file is part of Runway SDK(tm).
  *
- * Runway SDK(tm) is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * Runway SDK(tm) is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
  *
- * Runway SDK(tm) is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * Runway SDK(tm) is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with Runway SDK(tm).  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Runway SDK(tm). If not, see <http://www.gnu.org/licenses/>.
  */
 package com.runwaysdk.mvc.conversion;
+
+import org.json.JSONException;
 
 import com.runwaysdk.business.MutableDTO;
 import com.runwaysdk.mvc.JsonConfiguration;
@@ -34,6 +36,15 @@ public abstract class MutableDTOToBasicJSON extends ComponentDTOIFToBasicJSON
   protected MutableDTOToBasicJSON(MutableDTO mutableDTO, JsonConfiguration configuration)
   {
     super(mutableDTO, configuration);
+  }
+
+  @Override
+  protected void setProperties() throws JSONException
+  {
+    super.setProperties();
+
+    // id
+    this.getJSON().put("newInstance", this.getComponentDTO().isNewInstance());
   }
 
   /**
