befd5068820b0bad3372967cc7be8070c7f84251
terraframe-builder <builder@terraframe.com>
2018-02-07 16:10:20 +0000

    License headers

--- runwaysdk-server/src/main/resources/domain/(0001517987323813)ExecutableJobRunAsDimension.sql
+++ runwaysdk-server/src/main/resources/domain/(0001517987323813)ExecutableJobRunAsDimension.sql
@@ -1,3 +1,22 @@
+--
+-- Copyright (c) 2015 TerraFrame, Inc. All rights reserved.
+--
+-- This file is part of Runway SDK(tm).
+--
+-- Runway SDK(tm) is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Lesser General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+--
+-- Runway SDK(tm) is distributed in the hope that it will be useful, but
+-- WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Lesser General Public License for more details.
+--
+-- You should have received a copy of the GNU Lesser General Public
+-- License along with Runway SDK(tm).  If not, see <http://www.gnu.org/licenses/>.
+--
+
 UPDATE metadata SET seq= 4923 , last_update_date= '2018-02-07 00:40:01'  WHERE id='8ndv254jvk1mw1rql6a4pqz9qbqvdzl400000000000000000000000000000001';
 INSERT INTO md_attribute_indicies (set_id, item_id)  VALUES  ('xeribdhgj94gvi9r69qa7i5158q0wrui', '0000000000000000000000000000040000000000000000000000000000000403');
 INSERT INTO visibilitymodifier (set_id, item_id)  VALUES  ('1v1h9ukjtleg1evvc43ccjswj35gmpux', '20071109NM000000000000000000000520071109NM0000000000000000000001');
