ce909f584dc51760a191d8c02878393d553d0696
rrowlands <rrowlands@terraframe.com>
2018-04-30 11:26:41 -0600

    Added RedirectResponse to runway mvc.

---
+++ runwaysdk-client/src/main/java/com/runwaysdk/mvc/RedirectResponse.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2015 TerraFrame, Inc. All rights reserved.
+ *
+ * This file is part of Runway SDK(tm).
+ *
+ * Runway SDK(tm) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Runway SDK(tm) is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Runway SDK(tm).  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.runwaysdk.mvc;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import com.runwaysdk.controller.RequestManager;
+import com.runwaysdk.request.ServletRequestIF;
+import com.runwaysdk.request.ServletResponseIF;
+
+public class RedirectResponse implements ResponseIF
+{
+  private String url;
+  
+  private boolean addContext;
+  
+  public RedirectResponse(String url)
+  {
+    this.url = url;
+    this.addContext = true;
+  }
+  
+  public RedirectResponse(String url, boolean addContextPath)
+  {
+    this.url = url;
+    this.addContext = addContextPath;
+  }
+  
+  @Override
+  public void handle(RequestManager manager) throws ServletException, IOException
+  {
+    ServletRequestIF req = manager.getReq();
+    ServletResponseIF resp = manager.getResp();
+    
+    String finalUrl = this.url;
+    
+    if (this.addContext)
+    {
+      if (this.url.startsWith("/"))
+      {
+        finalUrl = req.getContextPath() + this.url;
+      }
+      else
+      {
+        finalUrl = req.getContextPath() + "/" + this.url;
+      }
+    }
+    
+    resp.sendRedirect(finalUrl);
+  }
+}
