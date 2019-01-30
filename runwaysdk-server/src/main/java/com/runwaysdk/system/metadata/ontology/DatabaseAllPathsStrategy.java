d8e2f7eb07b12070c7a9bc6bb7285c74dca66843
Justin Smethie <jsmethie@terraframe.com>
2019-01-30 09:31:29 -0700

    Truncate on DatabaseAllPathsStrategy shutdown

--- runwaysdk-server/src/main/java/com/runwaysdk/system/metadata/ontology/DatabaseAllPathsStrategy.java
+++ runwaysdk-server/src/main/java/com/runwaysdk/system/metadata/ontology/DatabaseAllPathsStrategy.java
@@ -3,18 +3,18 @@
  *
  * This file is part of Runway SDK(tm).
  *
- * Runway SDK(tm) is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * Runway SDK(tm) is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
  *
- * Runway SDK(tm) is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * Runway SDK(tm) is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with Runway SDK(tm).  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Runway SDK(tm). If not, see <http://www.gnu.org/licenses/>.
  */
 package com.runwaysdk.system.metadata.ontology;
 
@@ -303,7 +303,7 @@ public class DatabaseAllPathsStrategy extends DatabaseAllPathsStrategyBase
       database.shutdown(parameters);
 
       // Truncate the allpaths table records
-      MdBusiness.get(table.getOid()).deleteAllTableRecords();
+      MdBusinessDAO.get(table.getOid()).getBusinessDAO().deleteAllRecords();
 
       // Delete the termAllPaths MdBusiness
       table.delete();
@@ -327,6 +327,9 @@ public class DatabaseAllPathsStrategy extends DatabaseAllPathsStrategyBase
 
       OntologyDatabase database = new OntologyDatabaseFactory().getInstance(Database.instance(), this);
       database.shutdown(parameters);
+
+      // Truncate the allpaths table records
+      MdBusinessDAO.get(table.getOid()).getBusinessDAO().deleteAllRecords();
 
       // Delete the termAllPaths MdBusiness
       table.delete();
