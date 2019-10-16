ce102cf757cb6ddc9ed897b97afdea3312eea684
Justin Smethie <jsmethie@terraframe.com>
2019-10-16 14:12:05 -0600

    Test bad set type

--- runwaysdk-test/src/main/java/com/runwaysdk/dataaccess/graph/VertexObjectTest.java
+++ runwaysdk-test/src/main/java/com/runwaysdk/dataaccess/graph/VertexObjectTest.java
@@ -1,8 +1,14 @@
 package com.runwaysdk.dataaccess.graph;
 
+import java.util.Date;
+
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.runwaysdk.AttributeCharacterParseException;
+import com.runwaysdk.AttributeIntegerParseException;
 import com.runwaysdk.dataaccess.AttributeIF;
 import com.runwaysdk.dataaccess.io.TestFixtureFactory;
 import com.runwaysdk.dataaccess.metadata.MdAttributeBooleanDAO;
@@ -19,220 +25,264 @@ import com.runwaysdk.session.Request;
 
 public class VertexObjectTest
 {
+  private static MdVertexDAO             mdVertexDAO;
+
+  private static MdAttributeCharacterDAO mdCharacterAttribute;
+
+  private static MdAttributeIntegerDAO   mdIntegerAttribute;
+
+  private static MdAttributeLongDAO      mdLongAttribute;
+
+  private static MdAttributeFloatDAO     mdFloatAttribute;
+
+  private static MdAttributeDoubleDAO    mdDoubleAttribute;
+
+  private static MdAttributeBooleanDAO   mdBooleanAttribute;
+
+  private static MdAttributeDateDAO      mdDateAttribute;
+
+  private static MdAttributeDateTimeDAO  mdDateTimeAttribute;
+
+  private static MdAttributeTimeDAO      mdTimeAttribute;
+
+  @Request
+  @BeforeClass
+  public static void classSetup()
+  {
+    mdVertexDAO = TestFixtureFactory.createMdVertex();
+    mdVertexDAO.apply();
+
+    mdCharacterAttribute = TestFixtureFactory.addCharacterAttribute(mdVertexDAO);
+    mdCharacterAttribute.apply();
+
+    mdIntegerAttribute = TestFixtureFactory.addIntegerAttribute(mdVertexDAO);
+    mdIntegerAttribute.apply();
+
+    mdLongAttribute = TestFixtureFactory.addLongAttribute(mdVertexDAO);
+    mdLongAttribute.apply();
+
+    mdFloatAttribute = TestFixtureFactory.addFloatAttribute(mdVertexDAO);
+    mdFloatAttribute.apply();
+
+    mdDoubleAttribute = TestFixtureFactory.addDoubleAttribute(mdVertexDAO);
+    mdDoubleAttribute.apply();
+
+    mdBooleanAttribute = TestFixtureFactory.addBooleanAttribute(mdVertexDAO);
+    mdBooleanAttribute.apply();
+
+    mdDateAttribute = TestFixtureFactory.addDateAttribute(mdVertexDAO);
+    mdDateAttribute.apply();
+
+    mdDateTimeAttribute = TestFixtureFactory.addDateTimeAttribute(mdVertexDAO);
+    mdDateTimeAttribute.apply();
+
+    mdTimeAttribute = TestFixtureFactory.addTimeAttribute(mdVertexDAO);
+    mdTimeAttribute.apply();
+  }
+
+  @Request
+  @AfterClass
+  public static void classTearDown()
+  {
+    TestFixtureFactory.delete(mdVertexDAO);
+  }
+
   @Request
   @Test
   public void testCharacterAttribute()
   {
-    MdVertexDAO mdVertexDAO = TestFixtureFactory.createMdVertex();
-    mdVertexDAO.apply();
+    VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
 
-    try
-    {
-      MdAttributeCharacterDAO mdCharacterAttribute = TestFixtureFactory.addCharacterAttribute(mdVertexDAO);
-      mdCharacterAttribute.apply();
+    AttributeIF test = vertexDAO.getAttributeIF(mdCharacterAttribute.definesAttribute());
 
-      VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
+    Assert.assertNotNull(test);
 
-      AttributeIF test = vertexDAO.getAttributeIF(mdCharacterAttribute.definesAttribute());
-
-      Assert.assertNotNull(test);
-    }
-    finally
-    {
-      mdVertexDAO.delete();
-    }
+    vertexDAO.setValue(mdCharacterAttribute.definesAttribute(), "Test Value");
   }
 
   @Request
   @Test
   public void testIntegerAttribute()
   {
-    MdVertexDAO mdVertexDAO = TestFixtureFactory.createMdVertex();
-    mdVertexDAO.apply();
+    VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
 
-    try
-    {
-      MdAttributeIntegerDAO mdIntegerAttribute = TestFixtureFactory.addIntegerAttribute(mdVertexDAO);
-      mdIntegerAttribute.apply();
+    AttributeIF test = vertexDAO.getAttributeIF(mdIntegerAttribute.definesAttribute());
 
-      VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
+    Assert.assertNotNull(test);
 
-      AttributeIF test = vertexDAO.getAttributeIF(mdIntegerAttribute.definesAttribute());
-
-      Assert.assertNotNull(test);
-    }
-    finally
-    {
-      mdVertexDAO.delete();
-    }
+    vertexDAO.setValue(mdIntegerAttribute.definesAttribute(), new Integer(5));
   }
 
   @Request
   @Test
   public void testLongAttribute()
   {
-    MdVertexDAO mdVertexDAO = TestFixtureFactory.createMdVertex();
-    mdVertexDAO.apply();
+    VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
 
-    try
-    {
-      MdAttributeLongDAO mdLongAttribute = TestFixtureFactory.addLongAttribute(mdVertexDAO);
-      mdLongAttribute.apply();
+    AttributeIF test = vertexDAO.getAttributeIF(mdLongAttribute.definesAttribute());
 
-      VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
+    Assert.assertNotNull(test);
 
-      AttributeIF test = vertexDAO.getAttributeIF(mdLongAttribute.definesAttribute());
-
-      Assert.assertNotNull(test);
-    }
-    finally
-    {
-      mdVertexDAO.delete();
-    }
+    vertexDAO.setValue(mdLongAttribute.definesAttribute(), new Long(5));
   }
 
   @Request
   @Test
   public void testFloatAttribute()
   {
-    MdVertexDAO mdVertexDAO = TestFixtureFactory.createMdVertex();
-    mdVertexDAO.apply();
+    VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
 
-    try
-    {
-      MdAttributeFloatDAO mdFloatAttribute = TestFixtureFactory.addFloatAttribute(mdVertexDAO);
-      mdFloatAttribute.apply();
+    AttributeIF test = vertexDAO.getAttributeIF(mdFloatAttribute.definesAttribute());
 
-      VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
+    Assert.assertNotNull(test);
 
-      AttributeIF test = vertexDAO.getAttributeIF(mdFloatAttribute.definesAttribute());
-
-      Assert.assertNotNull(test);
-    }
-    finally
-    {
-      mdVertexDAO.delete();
-    }
+    vertexDAO.setValue(mdFloatAttribute.definesAttribute(), new Float(5F));
   }
 
   @Request
   @Test
   public void testDoubleAttribute()
   {
-    MdVertexDAO mdVertexDAO = TestFixtureFactory.createMdVertex();
-    mdVertexDAO.apply();
+    VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
 
-    try
-    {
-      MdAttributeDoubleDAO mdDoubleAttribute = TestFixtureFactory.addDoubleAttribute(mdVertexDAO);
-      mdDoubleAttribute.apply();
+    AttributeIF test = vertexDAO.getAttributeIF(mdDoubleAttribute.definesAttribute());
 
-      VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
+    Assert.assertNotNull(test);
 
-      AttributeIF test = vertexDAO.getAttributeIF(mdDoubleAttribute.definesAttribute());
-
-      Assert.assertNotNull(test);
-    }
-    finally
-    {
-      mdVertexDAO.delete();
-    }
+    vertexDAO.setValue(mdDoubleAttribute.definesAttribute(), new Double(5D));
   }
 
   @Request
   @Test
   public void testBooleanAttribute()
   {
-    MdVertexDAO mdVertexDAO = TestFixtureFactory.createMdVertex();
-    mdVertexDAO.apply();
+    VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
 
-    try
-    {
-      MdAttributeBooleanDAO mdBooleanAttribute = TestFixtureFactory.addBooleanAttribute(mdVertexDAO);
-      mdBooleanAttribute.apply();
+    AttributeIF test = vertexDAO.getAttributeIF(mdBooleanAttribute.definesAttribute());
 
-      VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
+    Assert.assertNotNull(test);
 
-      AttributeIF test = vertexDAO.getAttributeIF(mdBooleanAttribute.definesAttribute());
-
-      Assert.assertNotNull(test);
-    }
-    finally
-    {
-      mdVertexDAO.delete();
-    }
+    vertexDAO.setValue(mdBooleanAttribute.definesAttribute(), Boolean.TRUE);
   }
 
   @Request
   @Test
   public void testDateAttribute()
   {
-    MdVertexDAO mdVertexDAO = TestFixtureFactory.createMdVertex();
-    mdVertexDAO.apply();
+    VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
 
-    try
-    {
-      MdAttributeDateDAO mdDateAttribute = TestFixtureFactory.addDateAttribute(mdVertexDAO);
-      mdDateAttribute.apply();
+    AttributeIF test = vertexDAO.getAttributeIF(mdDateAttribute.definesAttribute());
 
-      VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
+    Assert.assertNotNull(test);
 
-      AttributeIF test = vertexDAO.getAttributeIF(mdDateAttribute.definesAttribute());
-
-      Assert.assertNotNull(test);
-    }
-    finally
-    {
-      mdVertexDAO.delete();
-    }
+    vertexDAO.setValue(mdDateAttribute.definesAttribute(), new Date());
   }
 
   @Request
   @Test
   public void testDateTimeAttribute()
   {
-    MdVertexDAO mdVertexDAO = TestFixtureFactory.createMdVertex();
-    mdVertexDAO.apply();
 
-    try
-    {
-      MdAttributeDateTimeDAO mdDateTimeAttribute = TestFixtureFactory.addDateTimeAttribute(mdVertexDAO);
-      mdDateTimeAttribute.apply();
+    VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
 
-      VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
+    AttributeIF test = vertexDAO.getAttributeIF(mdDateTimeAttribute.definesAttribute());
 
-      AttributeIF test = vertexDAO.getAttributeIF(mdDateTimeAttribute.definesAttribute());
+    Assert.assertNotNull(test);
 
-      Assert.assertNotNull(test);
-    }
-    finally
-    {
-      mdVertexDAO.delete();
-    }
+    vertexDAO.setValue(mdDateTimeAttribute.definesAttribute(), new Date());
   }
 
   @Request
   @Test
   public void testTimeAttribute()
   {
-    MdVertexDAO mdVertexDAO = TestFixtureFactory.createMdVertex();
-    mdVertexDAO.apply();
+    VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
 
-    try
-    {
-      MdAttributeTimeDAO mdTimeAttribute = TestFixtureFactory.addTimeAttribute(mdVertexDAO);
-      mdTimeAttribute.apply();
+    AttributeIF test = vertexDAO.getAttributeIF(mdTimeAttribute.definesAttribute());
 
-      VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
+    Assert.assertNotNull(test);
 
-      AttributeIF test = vertexDAO.getAttributeIF(mdTimeAttribute.definesAttribute());
-
-      Assert.assertNotNull(test);
-    }
-    finally
-    {
-      mdVertexDAO.delete();
-    }
+    vertexDAO.setValue(mdTimeAttribute.definesAttribute(), new Date());
   }
 
+  @Request
+  @Test(expected = AttributeCharacterParseException.class)
+  public void testBadCharacterAttribute()
+  {
+    VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
+
+    vertexDAO.setValue(mdCharacterAttribute.definesAttribute(), new Integer(4));
+  }
+
+  @Request
+  @Test(expected = AttributeIntegerParseException.class)
+  public void testBadIntegerAttribute()
+  {
+    VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
+
+    vertexDAO.setValue(mdIntegerAttribute.definesAttribute(), "Test Value");
+  }
+
+  @Request
+  @Test(expected = AttributeIntegerParseException.class)
+  public void testBadLongAttribute()
+  {
+    VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
+
+    vertexDAO.setValue(mdLongAttribute.definesAttribute(), "Test Value");
+  }
+
+  @Request
+  @Test(expected = AttributeIntegerParseException.class)
+  public void testBadFloatAttribute()
+  {
+    VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
+
+    vertexDAO.setValue(mdFloatAttribute.definesAttribute(), "Test Value");
+  }
+
+  @Request
+  @Test(expected = AttributeIntegerParseException.class)
+  public void testBadDoubleAttribute()
+  {
+    VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
+
+    vertexDAO.setValue(mdDoubleAttribute.definesAttribute(), "Test Value");
+  }
+
+  @Request
+  @Test(expected = AttributeIntegerParseException.class)
+  public void testBadBooleanAttribute()
+  {
+    VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
+
+    vertexDAO.setValue(mdBooleanAttribute.definesAttribute(), "Test Value");
+  }
+
+  @Request
+  @Test(expected = AttributeIntegerParseException.class)
+  public void testBadDateAttribute()
+  {
+    VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
+
+    vertexDAO.setValue(mdDateAttribute.definesAttribute(), "Test Value");
+  }
+
+  @Request
+  @Test(expected = AttributeIntegerParseException.class)
+  public void testBadDateTimeAttribute()
+  {
+    VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
+
+    vertexDAO.setValue(mdDateTimeAttribute.definesAttribute(), "Test Value");
+  }
+
+  @Request
+  @Test(expected = AttributeIntegerParseException.class)
+  public void testBadTimeAttribute()
+  {
+    VertexObjectDAO vertexDAO = VertexObjectDAO.newInstance(mdVertexDAO.definesType());
+
+    vertexDAO.setValue(mdTimeAttribute.definesAttribute(), "Test Value");
+  }
 }
