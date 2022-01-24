dbf893d775e9b63447a7a69601de5ea3c654ae8f
jsmethie <jsmethie@terraframe.com>
2022-01-24 15:47:21 -0700

    Added description to AbstractClassification

--- runwaysdk-server/src/main/java/com/runwaysdk/system/AbstractClassificationBase.java
+++ runwaysdk-server/src/main/java/com/runwaysdk/system/AbstractClassificationBase.java
@@ -1,24 +1,6 @@
-/**
- * Copyright (c) 2022 TerraFrame, Inc. All rights reserved.
- *
- * This file is part of Runway SDK(tm).
- *
- * Runway SDK(tm) is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * Runway SDK(tm) is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with Runway SDK(tm).  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.runwaysdk.system;
 
-@com.runwaysdk.business.ClassSignature(hash = -691042304)
+@com.runwaysdk.business.ClassSignature(hash = 1145753720)
 /**
  * This class is generated automatically.
  * DO NOT MAKE CHANGES TO IT - THEY WILL BE OVERWRITTEN
@@ -29,14 +11,31 @@ package com.runwaysdk.system;
 public abstract class AbstractClassificationBase extends com.runwaysdk.business.graph.VertexObject
 {
   public final static String CLASS = "com.runwaysdk.system.AbstractClassification";
+  public static java.lang.String DESCRIPTION = "description";
   public static java.lang.String DISPLAYLABEL = "displayLabel";
   public static java.lang.String OID = "oid";
   public static java.lang.String SEQ = "seq";
-  private static final long serialVersionUID = -691042304;
+  private static final long serialVersionUID = 1145753720;
   
   public AbstractClassificationBase()
   {
     super();
+  }
+  
+  public com.runwaysdk.ComponentIF getDescription()
+  {
+    return (com.runwaysdk.ComponentIF) this.getObjectValue(DESCRIPTION);
+  }
+  
+  public static com.runwaysdk.dataaccess.MdAttributeDAOIF getDescriptionMd()
+  {
+    com.runwaysdk.dataaccess.MdClassDAOIF mdClassIF = com.runwaysdk.dataaccess.metadata.MdClassDAO.getMdClassDAO(com.runwaysdk.system.AbstractClassification.CLASS);
+    return (com.runwaysdk.dataaccess.MdAttributeDAOIF)mdClassIF.definesAttribute(DESCRIPTION);
+  }
+  
+  public void setDescription(com.runwaysdk.ComponentIF value)
+  {
+    this.setValue(DESCRIPTION, value);
   }
   
   public com.runwaysdk.ComponentIF getDisplayLabel()
